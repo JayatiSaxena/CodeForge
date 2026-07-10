7112105b6ffd2cc1f914af05de87977b082e760b
Jayati Saxena <jayatisaxena06@gmail.com>
2026-07-10 11:41:56 +0530

    Test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/HomePage/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ResetPassword from "./pages/ResetPassword";
-import UpdatePassword from "./pages/UpdatePassword";
-import DashboardPage from "./pages/DashboardPage/DashboardPage";
-
-import Courses from "./components/Courses/Courses";
-import CourseDetails from "./components/Courses/CourseDetails";
-
-import ProtectedRoute from "./components/ProtectedRoute";
-import { AuthProvider } from "./context/AuthContext";
-
 function App() {
   return (
-     <BrowserRouter basename="/CodeForge">
-      <AuthProvider>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/update-password" element={<UpdatePassword />} />
-
-          {/* Protected Routes */}
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <DashboardPage />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/courses"
-            element={
-              <ProtectedRoute>
-                <Courses />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/courses/:id"
-            element={
-              <ProtectedRoute>
-                <CourseDetails />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <h1 style={{ textAlign: "center", marginTop: "100px" }}>
+      CodeForge is working 🚀
+    </h1>
   );
 }
 
